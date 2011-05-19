8241ab5ef578858ec34adaa23dc12205e57ad117
Matthieu Clochard <clochard.matthieu@gmail.com>
2011-05-19 10:16:44 +0200

    Add license headers for extension API

--- cdi-osgi-parent/cdi-osgi-core/cdi-osgi-extension-api/src/main/java/org/osgi/cdi/api/extension/annotation/Sent.java
+++ cdi-osgi-parent/cdi-osgi-core/cdi-osgi-extension-api/src/main/java/org/osgi/cdi/api/extension/annotation/Sent.java
@@ -1,11 +1,23 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.osgi.cdi.api.extension.annotation;
 
-import static java.lang.annotation.ElementType.PARAMETER;
-
+import javax.inject.Qualifier;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import javax.inject.Qualifier;
+
+import static java.lang.annotation.ElementType.PARAMETER;
 
 /**
  * <p>Qualifies an injection point for a event communication from outside the current bundle.</p>
