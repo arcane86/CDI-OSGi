b74fb32132823a4b03ebb82e1af144abca42b134
Matthieu Clochard <clochard.matthieu@gmail.com>
2011-05-19 15:21:10 +0200

    Add integration API files

---
+++ cdi-osgi-parent/cdi-osgi-core/cdi-osgi-integration-api/src/main/java/org/osgi/cdi/api/integration/CDIContainers.java
@@ -0,0 +1,10 @@
+package org.osgi.cdi.api.integration;
+
+/**
+ *
+ * @author Mathieu ANCELIN - SERLI (mathieu.ancelin@serli.com)
+ */
+public interface CDIContainers extends Iterable<CDIContainer> {
+
+}
+
