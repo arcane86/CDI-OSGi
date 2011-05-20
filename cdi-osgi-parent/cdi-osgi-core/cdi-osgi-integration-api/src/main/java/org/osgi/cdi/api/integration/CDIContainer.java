a7acbb6d1e944d3c72f6407c6c6f94c84116f57d
Matthieu Clochard <clochard.matthieu@gmail.com>
2011-05-20 11:29:10 +0200

    Refactor integration API and add Javadoc

--- cdi-osgi-parent/cdi-osgi-core/cdi-osgi-integration-api/src/main/java/org/osgi/cdi/api/integration/CDIContainer.java
+++ cdi-osgi-parent/cdi-osgi-core/cdi-osgi-integration-api/src/main/java/org/osgi/cdi/api/integration/CDIContainer.java
@@ -10,31 +10,134 @@ import javax.enterprise.inject.spi.BeanManager;
 import java.util.Collection;
 
 /**
+ * <p>This interface represents an iterable list of CDI containers used by
+ * CDI-OSGi.</p>
+ * <p>It allows to: <ul>
+ * <li>
+ * <p>Navigate through the list of CDI containers as an
+ * {@link Iterable},</p>
+ * </li>
+ * <li>
+ * <p>Obtain the number of CDI containers,</p>
+ * </li>
+ * <li>
+ * <p>Select a specific container by its bundle,</p>
+ * </li>
+ * <li>
+ * <p>Start and stop the selected CDI container,</p>
+ * </li>
+ * <li>
+ * <p>Obtain the state of the selected CDI container,</p>
+ * </li>
+ * <li>
+ * <p>Obtain the corresponding {@link Bundle},
+ * {@link BeanManager}, {@link Event}, managed bean
+ * {@link Class} and {@link Instance} and registred
+ * service as {@link ServiceRegistration},</p>
+ * </li>
+ * <li>
+ * <p>Fire {@link InterBundleEvent}.</p>
+ * </li>
+ * </ul></p>
  *
  * @author Mathieu ANCELIN - SERLI (mathieu.ancelin@serli.com)
+ * @author Matthieu CLOCHARD - SERLI (matthieu.clochard@serli.com)
+ * @see CDIContainerFactory
+ * @see Iterable
+ * @see org.osgi.framework.Bundle
+ * @see BeanManager
+ * @see Instance
+ * @see Event
+ * @see ServiceRegistration
+ * @see InterBundleEvent
  */
-public interface CDIContainer {
+public interface CDIContainer extends  Iterable<CDIContainer> {
 
+    /**
+     * Initialize the CDI container.
+     * @return true if the CDI container is initialized, false if anything goes wrong.
+     */
     boolean initialize();
 
+    /**
+     * Shutdown the CDI container.
+     * @return true if the CDI container is off, false if anything goes wrong.
+     */
     boolean shutdown();
 
+    /**
+     * Test if the CDI container is on and initialized.
+     * @return true if the CDI container is started, false otherwise.
+     */
     boolean isStarted();
 
+    /**
+     * Fire an {@link InterBundleEvent} from the {@link Bundle} of this {@link CDIContainer}.
+     * @param event the {@link InterBundleEvent} to fire.
+     */
     void fire(InterBundleEvent event);
 
+    /**
+     * Obtain the {@link Bundle} corresponding to this {@link CDIContainer}.
+     * @return the {@link Bundle} corresponding to this {@link CDIContainer}.
+     */
     Bundle getBundle();
 
+    /**
+     * Obtain the {@link BeanManager} of this {@link CDIContainer}.
+     * @return the {@link BeanManager} of this {@link CDIContainer}.
+     */
     BeanManager getBeanManager();
 
+    /**
+     * Obtain the {@link Event} of this {@link CDIContainer}.
+     * @return the {@link Event} of this {@link CDIContainer}.
+     */
     Event getEvent();
 
+    /**
+     * Obtain the managed bean {@link Instance} of this {@link CDIContainer}.
+     * @return the managed bean {@link Instance} of this {@link CDIContainer}.
+     */
     Instance<Object> getInstance();
 
+    /**
+     * Obtain the managed bean class of this {@link CDIContainer}.
+     * @return the managed bean class of this {@link CDIContainer} as a {@link Collection} of {@link String}.
+     */
     Collection<String> getBeanClasses();
 
+    /**
+     * Obtain the {@link ServiceRegistration}s of this {@link CDIContainer}.
+     * @return the {@link ServiceRegistration}s of this {@link CDIContainer} as a {@link Collection}.
+     */
     Collection<ServiceRegistration> getRegistrations();
 
+    /**
+     * Set the {@link ServiceRegistration}s for this {@link CDIContainer}.
+     * @param registrations the {@link ServiceRegistration}s for this {@link CDIContainer} as a {@link Collection}.
+     */
     void setRegistrations(Collection<ServiceRegistration> registrations);
+
+    /**
+     * Obtain the {@link CDIContainer} for the given {@link Bundle}.
+     * @param bundle the {@link Bundle} which {@link CDIContainer} is wanted.
+     * @return the {@link CDIContainer} for the given {@link Bundle}.
+     */
+    CDIContainer select(Bundle bundle);
+
+    /**
+     * Obtain the {@link CDIContainer} for the given {@link Bundle}.
+     * @param name symbolic name of the {@link Bundle} which {@link CDIContainer} is wanted.
+     * @param version the version of the {@link Bundle} which {@link CDIContainer} is wanted.
+     * @return the {@link CDIContainer} for the given {@link Bundle}.
+     */
+    CDIContainer select(String name, String version);
+
+    /**
+     * The number of {@link CDIContainer} in the current {@link CDIContainer} {@link Iterable} list.
+     * @return number of {@link CDIContainer}.
+     */
+    int size();
 
 }
--- cdi-osgi-parent/cdi-osgi-core/cdi-osgi-integration-api/src/main/java/org/osgi/cdi/api/integration/CDIContainerFactory.java
+++ cdi-osgi-parent/cdi-osgi-core/cdi-osgi-integration-api/src/main/java/org/osgi/cdi/api/integration/CDIContainerFactory.java
@@ -5,15 +5,50 @@ import org.osgi.framework.Bundle;
 import java.util.Set;
 
 /**
+ * <p>This interface represents a CDI container factory used by CDI-OSGi in order to
+ * obtain {@link CDIContainer}.</p>
+ * <p>It allows to: <ul>
+ * <li>
+ * <p>Obtain the CDI container of a specific bean {@link Bundle}
+ * (singleton for each bean bundle),</p>
+ * </li>
+ * <li>
+ * <p>Provide a interface black list for service publishing,</p>
+ * </li>
+ * <li>
+ * <p>Obtain the ID of the used CDI implementation.</p>
+ * </li>
+ * </ul></p>
+ *
  * @author Mathieu ANCELIN - SERLI (mathieu.ancelin@serli.com)
+ * @author Matthieu CLOCHARD - SERLI (matthieu.clochard@serli.com)
+ * @see CDIContainer
+ * @see org.osgi.framework.Bundle
  */
 public interface CDIContainerFactory {
 
-    Class<? extends CDIContainerFactory> delegateClass();
-
+    /**
+     * Obtain the ID of the used CDI implementation.
+     * @return the ID of the used CDI implementation.
+     */
     String getID();
 
+    /**
+     * Obtain the interface black list for service publishing,
+     * @return the interface black list for service publishing as a {@link java.util.List} of {@link String}.
+     */
     Set<String> getContractBlacklist();
 
+    /**
+     * Obtain the singleton {@link CDIContainer} for the given bundle.
+     * @param bundle the {@link Bundle} which {@link CDIContainer} is wanted.
+     * @return the {@link CDIContainer} for the given {@link Bundle}.
+     */
     CDIContainer container(Bundle bundle);
+
+    /**
+     * Obtaint all {@link CDIContainer}s.
+     * @return all {@link CDIContainer}s.
+     */
+    CDIContainer containers();
 }
