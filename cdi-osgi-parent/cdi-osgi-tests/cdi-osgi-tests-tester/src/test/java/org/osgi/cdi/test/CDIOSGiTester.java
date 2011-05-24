0f7142fc3f3029e496622eb2dd1401efbd5a85a4
Matthieu Clochard <clochard.matthieu@gmail.com>
2011-05-24 09:25:00 +0200

    Make tests to run

---
+++ cdi-osgi-parent/cdi-osgi-tests/cdi-osgi-tests-tester/src/test/java/org/osgi/cdi/test/CDIOSGiTester.java
@@ -0,0 +1,15 @@
+package org.osgi.cdi.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CDIOSGiTester {
+
+    @Test
+    public void test() {
+        Assert.fail("TEST");
+    }
+
+}
+
+
