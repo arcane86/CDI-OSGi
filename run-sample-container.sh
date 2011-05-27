47d7a49de06f9901cade880a6ce0e05162cd04db
Matthieu Clochard <clochard.matthieu@gmail.com>
2011-05-27 13:03:31 +0200

    Prepare version 1.0

--- run-sample-container.sh
+++ run-sample-container.sh
@@ -4,5 +4,5 @@
 clear;
 #export DEBUG_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,address=8000"
 #mvn clean install;
-cd ./cdi-osgi-sample/container/target/container-0.2-all/container-0.2/;
+cd ./cdi-osgi-sample/container/target/container-1.0-SNAPSHOT-all/container-1.0-SNAPSHOT/;
 java $DEBUG_OPTS -jar bin/felix.jar;
